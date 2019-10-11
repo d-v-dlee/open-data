515daa341c0224debc874d3a926fe0c00092de9b
David M. Lee <DMLee@Launchcg.com>
2019-10-10 18:18:28 -0700

    move unneeded code

--- scripts/event_helpers.py
+++ scripts/event_helpers.py
@@ -1,6 +1,9 @@
 import os
 import json
 import pandas as pd
+import string
+import numpy as np
+import pickle
 
 def group_by_event(json_, type_):
     """
@@ -40,6 +43,42 @@ def total_group_by_event(json_list, type_, inpath):
 
 
 ############ dataframe creator ##################
+def shot_location_numeric(shot_location_start):
+    """
+    takes a location that is a string '[103.0, 21.0]' and turn it into a list of floats [103.0, 21.0]
+    """
+    # pull only numbers and punctuation
+    numbers = ''.join(x for x in shot_location_start if x in string.digits or x in [',', '.'])
+    # split
+    number_list = numbers.split(',')
+    # turn into list of ints
+    final_location = [float(x) for x in number_list]
+
+    return final_location
+
+def transpose_shot(shot_location_start):
+    """
+    if a shot occurs past the halfway line, tranpose the shot in x and y.
+    NOTE: Unnecessary cause all shots are on the right side of the field...
+    """
+    if shot_location_start[0] > 60:
+        x = 120 - shot_location_start[0]
+        y = 80 - shot_location_start[1]
+        return [x, y]
+    else:
+        return shot_location_start
+
+def scale_to_visual(shot_location_start):
+    """
+    scales to fit visual
+    """
+    x_scale = 510 / 120
+    y_scale = 340/80
+
+    x = np.round(shot_location_start[0] * x_scale) 
+    y = np.round(shot_location_start[1] * y_scale)
+    return [x, y]
+
 def create_shot_dataframe(jsons, inpath, savepath):
     """
     pulls data from shot events and creates a df. that dataframe is then saved and returned
@@ -81,7 +120,8 @@ def create_shot_dataframe(jsons, inpath, savepath):
              'shot_location_start': shot_location_start, 'player': shot_player,
              'statsbomb_xg': xg, 'team': shot_team})
     
-    shots_df.to_csv(savepath + os.sep + 'shots_df.csv', index=False)
+    # save as pickle to maintain list in shot_location_start
+    shots_df.to_pickle(savepath + os.sep + 'shots_df.pkl')
 
     return shots_df
 
